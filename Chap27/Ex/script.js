2634b37a95cdf23f82153bfcbed496d664e36a0b
katswhatsup <118704052+katswhatsup@users.noreply.github.com>
2022-12-13 18:12:11 -0600

    second commit

--- Chap27/Ex/script.js
+++ Chap27/Ex/script.js
@@ -2,5 +2,20 @@ window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
         response.json().then(function(json) {
            console.log(json);
+
+
+            let indexNum = Math.floor(Math.random()*7);
+            let planets=document.getElementById("planets");
+            planets.innerHTML = `
+                <ul>
+                    <li>Name: ${json[indexNum].name}</li>
+                    <li>Diameter: ${json[indexNum].diameter}</li>
+                    <li>Star: ${json[indexNum].star}</li>
+                    <li>Distance: ${json[indexNum].distance}</li>
+                    <img style="width: 350px; height: 350px;" src="${json[indexNum].image}">
+                    <li>Moons: ${json[indexNum].moons}</li>
+                </ul>
+                `;
         });
  });
+});
---
+++ Chap27/Studio/script.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+   response.json().then(function(json) {
+});
